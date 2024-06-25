9d1cfb1e4767fb116728c9e222b2dfdd1670c69e
SergOrloff <6202818@gmail.com>
2024-06-25 20:57:48 +0300

    update main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_WEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WEIGHT))
+
+pygame.display.set_caption('Rifle-range game')
+icon = pygame.image.load('img/')
+
+
+
+
+
+running = True
+while running:
+    pass
+
+
+pygame.quit()
